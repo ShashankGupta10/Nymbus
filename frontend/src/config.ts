be13f8c979476417c66e2d8d0e5838622bbc3729
Shashank Gupta <shashankgupta9248@gmail.com>
2025-05-29 12:29:30 +0530

    :truck:: migrate backend from railway to render

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8080"
-export const BACKEND_URL = "https://nymbus-production.up.railway.app"
+export const BACKEND_URL = "https://nymbus.onrender.com"
